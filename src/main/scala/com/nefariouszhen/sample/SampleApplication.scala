5f97ba72a22ca7f93a16f725bcaa4cb59e8fb0f5
Nathaniel Bauernfeind <nate.bauernfeind@gmail.com>
2015-03-10 11:46:14 -0400

    Test out ConfigurableAssetsBundle.

---
+++ src/main/scala/com/nefariouszhen/sample/SampleApplication.scala
@@ -0,0 +1,31 @@
+package com.nefariouszhen.sample
+
+import javax.ws.rs.{GET, Path}
+
+import com.nefariouszhen.dropwizard.assets.{AssetsBundleConfiguration, AssetsConfiguration, ConfiguredAssetsBundle}
+import io.dropwizard.Configuration
+import io.dropwizard.setup.{Bootstrap, Environment}
+
+@Path("test")
+class SampleResource {
+  @GET
+  def get() = "Hello World"
+}
+
+class SampleConfiguration extends Configuration with AssetsBundleConfiguration {
+  override def getAssetsConfiguration: AssetsConfiguration = assetsConfiguration
+
+  val assetsConfiguration: AssetsConfiguration = new AssetsConfiguration
+}
+
+object SampleApplication extends ScalaApplication[SampleConfiguration] {
+  override def getName = "sample"
+
+  override def initialize(bootstrap: Bootstrap[SampleConfiguration]): Unit = {
+    bootstrap.addBundle(new ConfiguredAssetsBundle("/data/", "/assets/"))
+  }
+
+  def run(configuration: SampleConfiguration, environment: Environment): Unit = {
+    environment.jersey().register(new SampleResource)
+  }
+}
