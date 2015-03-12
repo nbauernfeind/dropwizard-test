1791a9ced032219c919a55b0e03cb97be59e0878
Nathaniel Bauernfeind <nate.bauernfeind@gmail.com>
2015-03-12 10:20:43 -0400

    Try to reproduce sclausen's issue.

--- src/main/scala/com/nefariouszhen/sample/SampleApplication.scala
+++ src/main/scala/com/nefariouszhen/sample/SampleApplication.scala
@@ -2,6 +2,7 @@ package com.nefariouszhen.sample
 
 import javax.ws.rs.{GET, Path}
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.nefariouszhen.dropwizard.assets.{AssetsBundleConfiguration, AssetsConfiguration, ConfiguredAssetsBundle}
 import io.dropwizard.Configuration
 import io.dropwizard.setup.{Bootstrap, Environment}
@@ -15,6 +16,7 @@ class SampleResource {
 class SampleConfiguration extends Configuration with AssetsBundleConfiguration {
   override def getAssetsConfiguration: AssetsConfiguration = assetsConfiguration
 
+  @JsonProperty("assets")
   val assetsConfiguration: AssetsConfiguration = new AssetsConfiguration
 }
 
@@ -22,7 +24,7 @@ object SampleApplication extends ScalaApplication[SampleConfiguration] {
   override def getName = "sample"
 
   override def initialize(bootstrap: Bootstrap[SampleConfiguration]): Unit = {
-    bootstrap.addBundle(new ConfiguredAssetsBundle("/data/", "/assets/"))
+    bootstrap.addBundle(new ConfiguredAssetsBundle())
   }
 
   def run(configuration: SampleConfiguration, environment: Environment): Unit = {
